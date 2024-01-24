6d0e552e231a8e099f776f6dc1ad7df004be2458
AlexandreLetscher <al.letscher@orange.fr>
2024-01-24 12:06:40 +0100

    modif person 2

--- src/Program.cs
+++ src/Program.cs
@@ -6,8 +6,14 @@ string Nom;
 
 
 Console.WriteLine("saisissez un prenom ");
+prenom =string.parse(console.readline());
+console.WriteLine("Saisissez le nom");
+nom = String.parse(console.readline());
+
+console.WriteLine(nom + prenom);
 
 
 
 
-console WriteLine( ")
+
+
